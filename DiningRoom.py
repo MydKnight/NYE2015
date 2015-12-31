85e72bb6d1c4b30ede01e89e769036c58b46565d
madsens <shiloh.madsen@gmail.com>
2015-12-31 12:47:38 -0800

    removed heartbeat from Dining room.

--- DiningRoom.py
+++ DiningRoom.py
@@ -10,7 +10,7 @@ print 'Starting'
 Movies.StartLoop('/home/pi/NYE2015/Assets/DiningRoom')
 
 while True:    # Runs until break is encountered. We want to set it to break on a particular ID.
-    Logging.HeartBeat()
+    #Logging.HeartBeat()
     n = raw_input("Scanned ID: ")
     if n == "STOP":
         Movies.StopLoop()
